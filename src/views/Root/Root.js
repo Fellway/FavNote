3e91f86ccbe67c59ee151778e5c9d8b51ad54fb4
Mateusz <mateusz.skrzypczyk1@hotmail.com>
2021-01-12 22:07:40 +0100

    Created basic project structure

---
+++ src/views/Root/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  border: 2px solid black;
+  padding: 50px;
+`;
+
+function App() {
+  return <Button>This is my button</Button>;
+}
+
+export default App;
