b3a10fd601a3675798192348a6a3966eed918674
Mateusz <mateusz.skrzypczyk1@hotmail.com>
2021-01-13 15:03:44 +0100

    Added ThemeProvider

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,18 +1,18 @@
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
+  background-color: ${({ theme }) => theme.primary};
   width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
-  font-weight: 600;
+  font-weight: ${({ theme }) => theme.bold};
   text-transform: uppercase;
 
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: hsl(0, 0%, 90%);
+      background-color: ${({ theme }) => theme.grey100};
       width: 105px;
       height: 30px;
       font-size: 10px;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Button from 'components/atoms/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/MainTheme';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <h1>Hello</h1>
-      <Button>Close / Save</Button>
-      <Button secondary>remove</Button>
+      <ThemeProvider theme={theme}>
+        <>
+          <h1>Hello</h1>
+          <Button>Close / Save</Button>
+          <Button secondary>remove</Button>
+        </>
+      </ThemeProvider>
     </div>
   );
 }
