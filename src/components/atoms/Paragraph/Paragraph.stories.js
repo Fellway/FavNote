01e640650c69dde1fe81e0ab9e810bfa2e15bebd
Mateusz <mateusz.skrzypczyk1@hotmail.com>
2021-01-14 23:58:41 +0100

    Added paragraph

---
+++ src/components/atoms/Paragraph/Paragraph.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Paragraph from './Paragraph';
+
+export default {
+  title: 'Components/Paragraph',
+  component: Paragraph,
+};
+
+export const Normal = () => (
+  <Paragraph>
+    Occaecat eiusmod elit sit ullamco id. Consequat nisi id laboris mollit et dolor commodo
+    voluptate veniam. Tempor do Lorem in commodo veniam sunt aliqua eiusmod do nisi ea culpa. Quis
+    ad officia deserunt incididunt laborum excepteur. Irure id sint incididunt magna aute
+    consectetur non. Duis ipsum magna consequat excepteur consectetur ad aute.
+  </Paragraph>
+);
